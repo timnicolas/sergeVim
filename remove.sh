73ddab87459cbf643f5b34fc87f02900d27c8055
tarama78 <tarama7810@gmail.com>
2017-11-25 22:28:44 +0100

    ok

--- remove.sh
+++ remove.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 rm -rf ~/.vimrc
 rm -rf ~/.vim
-rm -rf ~/.vimrc_backup
 rm -rf ~/.vim_backup
--- update_git.sh
+++ update_git.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 echo "update git repository"
-find . -iname ".vim_backup" --delete
 mkdir ~/.vim-backup
-cp ~/.vim ~/.vim_backup
-cp ~/.vimrc ~/.vimrc_backup
+cp sergeVim ~/.vim_backup
+cp vimrc ~/.vimrc_backup
+cp ~/.vimrc .
+cp -r ~/.vim/sergeVim .
 git add -A
 git commit -m "update git repository"
 git push
