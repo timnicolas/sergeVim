e9c7b353c2658f0f8fc99d4a3cec91084b20468b
Timothee NICOLAS <tnicolas@e3r6p3.42.fr>
2017-11-26 13:20:36 +0100

    update git repository

--- update_git.sh
+++ update_git.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-echo "\x1b[35m"
+echo -n "\x1b[35m"
 cat sergeheader
-echo "\x1b[0m"
+echo -n "\x1b[0m"
 echo "update git repository"
 mkdir -p ~/.vim-backup
 cp -r sergeVim ~/.vim_backup
