15cdc09f50678ab44a2b6de9b9d8804fcaa186c8
Timothee NICOLAS <tnicolas@e3r6p3.42.fr>
2017-11-26 10:10:17 +0100

    update install

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cp ~/.vimrc ~/.vimrc_old_config
 cp vimrc ~/.vimrc
+rm -rf ~/.vim
 mkdir -p ~/.vim
 cp -r sergeVim ~/.vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
