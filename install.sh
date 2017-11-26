9cc4c4225f50d9bd21016676d6775f5b3f115648
tim <tnicolas@student.42.fr>
2017-11-26 10:11:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 cp ~/.vimrc ~/.vimrc_old_config
 cp vimrc ~/.vimrc
-rm -rf ~/.vim
+if [ -d "~/.vim" ]; then
+  rm -rf ~/.vim
+fi
 mkdir -p ~/.vim
 cp -r sergeVim ~/.vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
