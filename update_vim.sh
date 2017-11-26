41dd00c94d26e1b2a1f4c6b3bb289c3a4793f4e8
tarama78 <tnicolas@student.42.fr>
2017-11-26 18:31:43 +0100

    update git repository

--- update_vim.sh
+++ update_vim.sh
@@ -6,7 +6,7 @@
 #    By: tnicolas <tnicolas@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/11/26 15:20:39 by tnicolas          #+#    #+#              #
-#    Updated: 2017/11/26 18:15:52 by tnicolas         ###   ########.fr        #
+#    Updated: 2017/11/26 18:30:01 by tnicolas         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,13 +16,13 @@ cat sergeHeader
 echo "\x1b[0m"
 echo "update vim config"
 git pull
-mkdir -p ~/.vim-backup
+mkdir -p ~/.vim_backup
 cp -r ~/.vim/sergeVim ~/.vim_backup
-cp ~/.vimrc ~/.vimrc_backup
+cp ~/.vimrc ~/.vim_backup/vimrc
 cp vimrc ~/.vimrc
 mkdir -p ~/.vim
 mkdir -p ~/.vim/sergeVim
-if [ "$1" == "--param" ]; then
+if [[ $# -eq 1 && "$1" == "--param" ]]; then
 	cp -r sergeVim ~/.vim
 else
 	cp -r `find sergeVim \( -iname "*" -a -not -iname "*param*" \) | grep "/"` ~/.vim/sergeVim
